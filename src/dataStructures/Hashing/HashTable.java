f1be66ef467e06aa4874c6225f486a727a68b922
Tommy Condon <20101841@mail.wit.ie>
2023-11-21 18:43:39 +0000

    Insert header for hashtable class

--- src/dataStructures/Hashing/HashTable.java
+++ src/dataStructures/Hashing/HashTable.java
@@ -5,6 +5,7 @@ public class HashTable<T> {
     /* Hash Table using Quadratic Probing technique */
     /* Uses primitive array to store objects of a specified type */
     /* Written by: Tommy Condon */
+    /* Open Addressing */
     T[] hashTable;
 
     public HashTable(int size){
